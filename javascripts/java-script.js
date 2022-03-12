873655d9b74af76cdd6d4dab02034c9b4708ad68
Hounsot <64931129+Hounsot@users.noreply.github.com>
2022-03-12 12:08:01 +0300

    First responsive screen

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,12 +1,4 @@
-let x;
-$(window).on('scroll', function() {
-	x = $(window).scrollTop();
-	$('.test').css('background-size', 100 + parseInt(x / 10) + '% ');
-}); // при скролле будет приближение, изменение размера
-// $(window) - внутри вьюпорта
-let y;
-$(window).on('scroll', function() {
-  y = $(window).scrollTop();
-	$('.kubik').css('left', 0 + parseInt(y / 10) + '% ');
-}); // при скролле будет приближение, изменение размера
-// $(window) - внутри вьюпорта
+$(document).ready(function() {
+  $("#clouds").animate({
+    });
+});
