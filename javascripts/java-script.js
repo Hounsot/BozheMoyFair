43992fdc6791674e47873ef260cb99751085413a
Hounsot <64931129+Hounsot@users.noreply.github.com>
2022-03-16 13:01:52 +0300

    qwe

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,19 +1,9 @@
-// $(document).ready(function() {
-//   $("#clouds").setTimeout({
-//     });
-// });
 $(document).ready(function(lol){
   $("#OpeningCont").css("display", "grid")
 });
-// $(document).ready(function(){
-//   $("#OpeningCont").("display", "none")
-// });
 $(document).ready(function(qol){
   $(document).delay(4000).queue(function(){
     $("#OpeningText").css("display", "grid");
     $("#OpeningCont").css("display", "none");
   });
 });
-// $(document).ready(function(){
-//   $("body").css("display", "none");
-// })
