ab47570dd73c105aeb7bfae45b570593ee16f60d
Hounsot <64931129+Hounsot@users.noreply.github.com>
2022-03-16 16:10:38 +0300

    LOADING SCREEN

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,9 +1,31 @@
-$(document).ready(function(lol){
-  $("#OpeningCont").css("display", "grid")
+$(document).ready(function(){
+  $(".loading").css("display", "flex");
 });
-$(document).ready(function(qol){
+
+$(document).ready(function(){
+  $(document).delay(3000).queue(function(){
+    $(".loading").toggleClass("transition");
+    $(this).dequeue();
+  });
+});
+$(document).ready(function(){
   $(document).delay(4000).queue(function(){
+    $(".loading").css("display", "none");
+    $("#OpeningCont").css("display", "grid");
+    $(this).dequeue();
+  });
+});
+
+$(document).ready(function(qol){
+  $(document).delay(8000).queue(function(){
     $("#OpeningText").css("display", "grid");
     $("#OpeningCont").css("display", "none");
   });
 });
+//
+// $(document).ready(function(qol){
+//   $(document).delay(4000).queue(function(){
+//     $("#OpeningText").css("display", "grid");
+//     $("#OpeningCont").css("display", "none");
+//   });
+// });
