1acce1a3cc1033c3a9a2106b3716179a6bb94e07
Hounsot <64931129+Hounsot@users.noreply.github.com>
2022-03-17 13:12:37 +0300

    Miras

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-  $(document).delay(3000).queue(function(){
+  $(document).delay(2500).queue(function(){
     $(".loading").toggleClass("transition");
     $(this).dequeue();
   });
 });
 $(document).ready(function(){
-  $(document).delay(4000).queue(function(){
+  $(document).delay(3000).queue(function(){
     $(".loading").css("display", "none");
     $("#OpeningCont").css("display", "grid");
     $(this).dequeue();
@@ -17,15 +17,16 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(qol){
-  $(document).delay(8000).queue(function(){
+  $(document).delay(6000).queue(function(){
     $("#OpeningText").css("display", "grid");
     $("#OpeningCont").css("display", "none");
   });
-});
-//
-// $(document).ready(function(qol){
-//   $(document).delay(4000).queue(function(){
-//     $("#OpeningText").css("display", "grid");
-//     $("#OpeningCont").css("display", "none");
-//   });
-// });
+$(document).ready(function(){
+  $(".button-wrap").click(function(){
+    $("#OpeningText").css("display", "none");
+    $("#BadResult").css("display", "grid");
+  })
+})
+$(document).ready(function(){
+  $(".")
+})
