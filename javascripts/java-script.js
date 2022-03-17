9f199a38acd6e41b6a9a5852d8f8b192017fbaac
Hounsot <64931129+Hounsot@users.noreply.github.com>
2022-03-17 14:05:33 +0300

    HelpMe

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,31 +1,31 @@
+$(document).ready(function(){
+  $(".loading").css("display", "flex");
+});
+
+$(document).ready(function(){
+  $(document).delay(2500).queue(function(){
+    $(".loading").toggleClass("transition");
+    $(this).dequeue();
+  });
+});
+$(document).ready(function(){
+  $(document).delay(3000).queue(function(){
+    $(".loading").css("display", "none");
+    $("#OpeningCont").css("display", "grid");
+    $(this).dequeue();
+  });
+});
+
+$(document).ready(function(){
+  $(document).delay(6000).queue(function(){
+    $("#OpeningText").css("display", "grid");
+    $("#OpeningCont").css("display", "none");
+    $(this).dequeue();
+  });
+});
 // $(document).ready(function(){
-//   $(".loading").css("display", "flex");
-// });
-//
-// $(document).ready(function(){
-//   $(document).delay(2500).queue(function(){
-//     $(".loading").toggleClass("transition");
-//     $(this).dequeue();
-//   });
-// });
-// $(document).ready(function(){
-//   $(document).delay(3000).queue(function(){
-//     $(".loading").css("display", "none");
-//     $("#OpeningCont").css("display", "grid");
-//     $(this).dequeue();
-//   });
-// });
-//
-// // $(document).ready(function(){
-// //   $(document).delay(6000).queue(function(){
-// //     $("#OpeningText").css("display", "grid");
-// //     $("#OpeningCont").css("display", "none");
-// //     $(this).dequeue();
-// //   });
-// // });
-// // $(document).ready(function(){
-// //   $(".button-wrap").click(function(){
-// //     $("#OpeningText").css("display", "none");
-// //     $("#BadResult").css("display", "grid");
-// //   })
-// // })
+//   $(".button-wrap").click(function(){
+//     $("#OpeningText").css("display", "none");
+//     $("#BadResult").css("display", "grid");
+//   })
+// })
