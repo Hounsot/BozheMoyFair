6b49e1fbbb6f65d7606462c562b5152ee8769f50
Hounsot <64931129+Hounsot@users.noreply.github.com>
2022-03-20 19:09:10 +0300

    game-mech

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -28,21 +28,71 @@ $(document).ready(function() {
   $(".button-wrap").click(function() {
     $("#OpeningText").css("display", "none");
     $("#game").css("display", "grid");
-    // var vpw = $(document).width();
-    // var leq = $('.fire').css('margin-left');
-    // console.log(vpw);
-    // console.log(leq);
-    // $('.fire').animate({ marginLeft: `${vpw}px`}, 500);
   })
 })
 
-var intervalId = window.setInterval(function(){
-  var vpw = $(document).width();
-  var leq = $('.fire').css('margin-left');
-  console.log(vpw);
-  console.log(leq);
-  $('.fire').animate({ marginLeft: `${vpw}px`}, 500);
-}, 5000);
+// $(document).ready(function(){
+//   $(".button-wrap").click(function(){
+//     var = intervalId = window.setInterval(function(z){
+//       $("#gameField").append('<img class="fire" src="images/Hellfire.svg" alt="">') 5000;
+//     })
+//   })
+// })
+// $(document).ready(function(){
+//   vpw =
+//   if () {
+//
+//   }
+// })
+
+// var counter = 0;
+// var viewportwidth = $(document).width();
+//   $(".button-wrap").click(function() {
+//     console.log(viewportwidth);
+//     var timer = setInterval(function() {
+//         counter++;
+//         $(".mech").append('<img class="fire" src="images/Hellfire.svg" alt="">');
+//         $(".fire").animate({ left: `${viewportwidth}px`}, 4000);
+//         // update(url, baseName(data));
+//         if(counter>=10){
+//             clearInterval(timer)
+//         }
+//     }, 1000);
+//   });
+
+$(".button-wrap").click(function(){
+  var counter = 0;
+  var timer = setInterval(function () {
+      counter++;
+      console.log(counter);
+      if(counter>=10){
+          clearInterval(timer)
+      }
+  }, 1000);
+})
+$(document).ready(function(){
+  $(".button-wrap").click(function(){
+    $(".mech").append('<div class="ghost"><img src="images/ghostface.svg" alt=""><div class="stick"></div></div>');
+  })
+})
+// <div class="ghost">
+//   <img src="ghostface.svg" alt="">
+//   <div class="stick">
+//   </div>
+// </div>
+//
+// $(document).ready(function(){
+//   $('.button-wrap').click(function(){
+//     var intervalId = window.setInterval(function(){
+//       var vpw = $(document).width();
+//       var leq = $('.fire').css('margin-left');
+//       console.log(vpw);
+//       console.log(leq);
+//       $('.fire').animate({ marginLeft: `${vpw}px`}, 3000);
+//       $(".hellFire").append('<img class="fire" src="images/Hellfire.svg" alt="">');
+//     }, 3000);
+//   })
+// })
 // $(document).ready(){
 //   $(this
 // }
