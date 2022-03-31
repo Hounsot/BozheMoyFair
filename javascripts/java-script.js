553e5425cad3a71dec8c6c67f84ddfb89bc68279
Hounsot <64931129+Hounsot@users.noreply.github.com>
2022-03-31 18:10:35 +0300

    BadFinal

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   $(".loading").css("display", "flex");
 });
-
+$(document).ready(function(){
+  $("#result").css("display", "none");
+})
 $(document).ready(function() {
   $(document).delay(2500).queue(function() {
     $(".loading").toggleClass("transition");
@@ -36,66 +38,17 @@ $(document).ready(function(){
     $("#smallerCloudsUp").animate({ marginTop: `${cloudHran}px`}, 3000);
   }, 3000);
 })
+$(document).ready(function(){
+  var timer = setInterval(function(){
+    var cloudHran = Math.random()*100 + 80;
+    $("#smallerCloudsDown").animate({ marginTop: `${cloudHran}px`}, 3000);
+  }, 3000);
+})
 
 
-// $(document).ready(function(){
-//   $(".button-wrap").click(function(){
-//     var = intervalId = window.setInterval(function(z){
-//       $("#gameField").append('<img class="fire" src="images/Hellfire.svg" alt="">') 5000;
-//     })
-//   })
-// })
-// $(document).ready(function(){
-//   vpw =
-//   if () {
-//
-//   }
-// })
-
-// var counter = 0;
-// var viewportwidth = $(document).width();
-//   $(".button-wrap").click(function() {
-//     console.log(viewportwidth);
-//     var timer = setInterval(function() {
-//         counter++;
-//         $(".mech").append('<img class="fire" src="images/Hellfire.svg" alt="">');
-//         $(".fire").animate({ left: `${viewportwidth}px`}, 4000);
-//         // update(url, baseName(data));
-//         if(counter>=10){
-//             clearInterval(timer)
-//         }
-//     }, 1000);
-//   });
-
-// $(".button-wrap").click(function(){
-//   var counter = 0;
-//   var timer = setInterval(function () {
-//       counter++;
-//       console.log(counter);
-//       if(counter>=10){
-//           clearInterval(timer)
-//       }
-//   }, 1000);
-// })
-
-// $(document).ready(function(){
-//   var viewportwidth = $(document).width();
-//   $(".button-wrap").click(function(){
-//     $(".mech").append('<div class="ghost"><img src="images/ghostface.svg" alt=""><div class="stick"></div></div>');
-//   });
-//   var counter = 0;
-//   var timer = setInterval(function(){
-//     counter++
-//     $(".ghost").animate({ left: `${viewportwidth}px`}, 20000);
-//     if(counter>=100){
-//       clearInterval(timer)
-//     }
-//   }, 1000);
-// })
 var pipik = '<div class="ghost"><img class="ghostimage" src="images/ghist.png" alt=""><div class="stick"></div></div>'
 $(document).ready(function(){
   var viewportwidth = $(document).width();
-  // var lol = $(".ghost").offset().left;
   var counter = 0;
   var speedinc = 0;
   var hithit = 0;
@@ -103,27 +56,27 @@ $(document).ready(function(){
   var miss = 0;
   $(".button-wrap").click(function(){
   var timer = setInterval(function(){
+    var fff = $("#result").css("display");
+    console.log(fff);
     let randomA = Math.random()*200+300;
     counter++;
     speedinc = speedinc + 50;
-    // console.log($(".ghost").offset())
-    // console.log(viewportwidth);
-    // console.log(randomA);
-    // console.log(counter);
-    // console.log(speedinc);
     if (hithit == 2) {
       $("#game").css("display", "none");
       $("#result").css("display", "grid");
+    }
+    if (fff === "grid") {
       $("#result").delay(3000).queue(function(){
-        if (hithit > 20) {
-          $("#result").css("display", "none");
+        if (hithit >= 20) {
           $("#goodFinal").css("display", "grid");
-        }
-        else {
           $("#result").css("display", "none");
+        } else {
           $("#badFinal").css("display", "grid");
+          $("#result").css("display", "none");
+
         }
-      });
+        $(this).dequeue();
+      })
     }
     $(".mech").append(pipik);
     $(".ghost").animate({ left: `${viewportwidth}px`,
@@ -134,15 +87,10 @@ $(document).ready(function(){
   this.remove();
   if (lol === viewportwidth) {
     miss += 1;
-    // console.log(lol);
     console.log("miss");
     this.remove();
   }
 },
-// function(){
-//     console.log("lol");
-//       this.remove();
-//     }
   );
     $(".ghost").click(function(){
       hithit++;
@@ -155,79 +103,9 @@ ${hithit}/${miss}`);
     if(counter>=100){
       clearInterval(timer)
     }
-  }, 2000)
+    else if (fff === "grid") {
+      clearInterval(timer)
+    }
+  }, 4000)
 })
 })
-
-// $(document).ready(function(){
-//   $("").delay(3000).queue(function(){
-//     if (hithit > 20) {
-//       $("#result").css("display", "none");
-//       $("#goodFinal").css("display", "grid");
-//     }
-//     else {
-//       $("#result").css("display", "none");
-//       $("#badFinal").css("display", "grid");
-//     }
-//   })
-// })
-
-// $(document).ready(function(){
-// var hit = 0
-// $(".ghost").click(function(){
-//   hit++;
-//   console.log(hit);
-//   $(".ghost").animate({opacity: 0}, 100, function(){
-//     this.remove();
-//   });
-// })
-// });
-// $(document).ready(function(){
-//   $(function () {
-//     let count = 0;
-//     $(".ghost").click(function () {
-//       count += 1;
-//       console.log(count);
-//       if (count == 1) {
-//         $(".ghost").css("opacity", "0");
-//       }
-//     });
-//   });
-// })
-
-// $(document).ready(function(){
-//   console.log("hello");
-//   $(".ghost").click(function(){
-//     console.log("privet");
-//   })
-// })
-
-//задаем функции условие
-
-// += присваивание
-// <div class="ghost">
-//   <img src="ghostface.svg" alt="">
-//   <div class="stick">
-//   </div>
-// </div>
-//
-// $(document).ready(function(){
-//   $('.button-wrap').click(function(){
-//     var intervalId = window.setInterval(function(){
-//       var vpw = $(document).width();
-//       var leq = $('.fire').css('margin-left');
-//       console.log(vpw);
-//       console.log(leq);
-//       $('.fire').animate({ marginLeft: `${vpw}px`}, 3000);
-//       $(".hellFire").append('<img class="fire" src="images/Hellfire.svg" alt="">');
-//     }, 3000);
-//   })
-// })
-// $(document).ready(){
-//   $(this
-// }
-// $(document).ready(function() {
-//   if (leq > vpw) {
-//     $(".hellFire").append('<img class="fire" src="images/Hellfire.svg" alt="">');
-//   }
-// })
